f1d47f6adbce4814422deb841119f4dbe2aef937
SamuelDarras <slamicote@gmail.com>
2022-09-29 10:47:35 +0200

    projet libgdx initial

---
+++ the-ascend-of-pluton/desktop/src/fr/ul/theascendofpluton/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package fr.ul.theascendofpluton;
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
+import fr.ul.theascendofpluton.Game;
+
+// Please note that on macOS your application needs to be started with the -XstartOnFirstThread JVM argument
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		Lwjgl3ApplicationConfiguration config = new Lwjgl3ApplicationConfiguration();
+		config.setForegroundFPS(60);
+		config.setTitle("The Ascend Of Pluton");
+		new Lwjgl3Application(new Game(), config);
+	}
+}
