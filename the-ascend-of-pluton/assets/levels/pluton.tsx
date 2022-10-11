99a272d92b192c092b02ff9395744be6fe9d6831
fxzoroark <fxzoroark@outlook.com>
2022-10-11 10:03:57 +0200

    Chargement d'un fichier niveau dans le jeu

---
+++ the-ascend-of-pluton/assets/levels/pluton.tsx
@@ -0,0 +1,23 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.2" name="pluton" tilewidth="32" tileheight="32" tilecount="180" columns="20">
+ <image source="../pluton_tileset.png" width="640" height="288"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="3">
+   <object id="6" x="32.9991" y="11.7609">
+    <polygon points="-0.685103,-0.114184 -18.9545,-0.114184 -18.8403,20.2105 -32.8849,20.2105 -32.9991,-11.7609 -0.570919,-11.7609"/>
+   </object>
+  </objectgroup>
+ </tile>
+ <tile id="1">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0.114184" y="0" width="31.9715" height="11.9893"/>
+  </objectgroup>
+ </tile>
+ <tile id="2">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-0.114184" y="11.8751">
+    <polygon points="0,0 20.3247,0 20.2105,19.9822 31.9715,20.0963 31.9715,-11.5326 0.228368,-11.7609"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
---
+++ the-ascend-of-pluton/core/src/fr/ul/theascendofpluton/LevelLoader.java
@@ -0,0 +1,29 @@
+package fr.ul.theascendofpluton;
+
+import com.badlogic.gdx.maps.MapLayer;
+import com.badlogic.gdx.maps.MapRenderer;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+
+public class LevelLoader {
+    private TiledMap tiledMap;
+    private TiledMapRenderer tiledMapRenderer;
+
+    public LevelLoader(){
+    }
+
+    public void load(String level_name){
+        tiledMap = new TmxMapLoader().load("levels/" + level_name + ".tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+    }
+
+    public MapRenderer getRenderer(){
+        return tiledMapRenderer;
+    }
+
+    public MapLayer getEntities(){
+        return tiledMap.getLayers().get("entities");
+    }
+}
