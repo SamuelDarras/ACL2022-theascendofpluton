8690da5a2b659d8d5db98a443a738a690f385a26
YohanFr37 <yohan.boucher.yb@gmail.com>
2022-11-04 18:27:49 +0100

    Ajout Acide et vie

---
+++ the-ascend-of-pluton/core/src/java/fr/ul/theascendofpluton/listener/MyContactListener.java
@@ -0,0 +1,31 @@
+package fr.ul.theascendofpluton.listener;
+
+import com.badlogic.gdx.physics.box2d.Contact;
+import com.badlogic.gdx.physics.box2d.ContactImpulse;
+import com.badlogic.gdx.physics.box2d.Manifold;
+
+public class ContactListener  implements ContactListener {
+
+    private B2dModel parent;
+
+    public B2dContactListener(B2dModel parent){
+        this.parent = parent;
+    }
+
+    @Override
+    public void beginContact(Contact contact) {
+    }
+
+    @Override
+    public void endContact(Contact contact) {
+    }
+
+    @Override
+    public void preSolve(Contact contact, Manifold oldManifold) {
+    }
+
+    @Override
+    public void postSolve(Contact contact, ContactImpulse impulse) {
+    }
+
+}
