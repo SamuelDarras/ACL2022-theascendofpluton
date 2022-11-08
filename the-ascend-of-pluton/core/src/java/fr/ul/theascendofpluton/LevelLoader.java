0891c3d30312ecf3fb78ba5c06a7a98005f75642
fxzoroark <fxzoroark@outlook.com>
2022-11-08 17:20:01 +0100

    ajout des textures et de l'acid puddle dans la tiledmap

--- the-ascend-of-pluton/core/src/java/fr/ul/theascendofpluton/LevelLoader.java
+++ the-ascend-of-pluton/core/src/java/fr/ul/theascendofpluton/LevelLoader.java
@@ -4,12 +4,15 @@ import com.badlogic.gdx.maps.*;
 import com.badlogic.gdx.maps.objects.PolygonMapObject;
 import com.badlogic.gdx.maps.tiled.*;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.math.Polygon;
 import com.badlogic.gdx.physics.box2d.World;
 import com.badlogic.gdx.utils.Array;
+import fr.ul.theascendofpluton.model.AcidPuddle;
 import fr.ul.theascendofpluton.model.Zombie;
 import fr.ul.theascendofpluton.model.Obstacle;
 
 import java.util.*;
+import java.util.Map;
 
 public class LevelLoader {
     private TiledMap tiledMap;
@@ -18,6 +21,10 @@ public class LevelLoader {
     public LevelLoader(){
     }
 
+    /**
+     * Charge la TiledMap et créé le renderer
+     * @param level_name
+     */
     public void load(String level_name){
         tiledMap = new TmxMapLoader().load("levels/" + level_name + ".tmx");
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
@@ -30,27 +37,60 @@ public class LevelLoader {
     public MapObject getPluton(){
         return tiledMap.getLayers().get("Player").getObjects().get("Pluton");
     }
-    private void addObstaclesFromLayer(TiledMapTileLayer mapLayer, TiledMapTileSet tiledMapTiles, World world){
-        System.out.println("toto");
 
+    /**
+     * récupère les coordonnées et les sommets des polygones présents dans une couche de la map
+     * @param mapLayer couche de la map contenant des tuiles
+     */
+    private Map<int[], Array<float[]>> getPolygonesFromLayer(TiledMapTileLayer mapLayer){
+        Map<int [], Array<float[]>> map = new HashMap<>();
         for (int i = 0; i < mapLayer.getWidth(); i++) {
             for(int j = 0; j < mapLayer.getHeight(); j++){
                 TiledMapTileLayer.Cell cell = mapLayer.getCell(i, j);
                 if (cell != null){
-                    Array<PolygonMapObject> polygons = cell.getTile().getObjects().getByType(PolygonMapObject.class);
-                    for (PolygonMapObject polygonMapObject : polygons){
-                        new Obstacle(world, i * mapLayer.getTileWidth(), j * mapLayer.getTileHeight(), polygonMapObject.getPolygon().getTransformedVertices());
+                    int[] coords = new int[]{i * mapLayer.getTileWidth(), j * mapLayer.getTileHeight()};
+                    Array<float[]> arrayVerticies = new Array<>();
+                    for (PolygonMapObject polygonMapObject : cell.getTile().getObjects().getByType(PolygonMapObject.class)){
+                        arrayVerticies.add(polygonMapObject.getPolygon().getTransformedVertices());
                     }
+                    map.put(coords, arrayVerticies);
                 }
             }
         }
-    }
-    public void addObstacles(World world){
-        TiledMapTileSet t = tiledMap.getTileSets().getTileSet("pluton");
-        addObstaclesFromLayer((TiledMapTileLayer)tiledMap.getLayers().get("sol"), t, world);
-        addObstaclesFromLayer((TiledMapTileLayer)tiledMap.getLayers().get("vide"), t, world);
+        return map;
     }
 
+    /**
+     * Ajoute les différents obstacles dans le monde via la tiledMap
+     * @param world
+     */
+    public void addObstacles(World world){
+        Map<int[], Array<float[]>> mapObstacles = new HashMap<>();
+        Map<int[], Array<float[]>> mapPuddles = new HashMap<>();
+
+        mapObstacles.putAll(getPolygonesFromLayer((TiledMapTileLayer)tiledMap.getLayers().get("sol")));
+        mapObstacles.putAll(getPolygonesFromLayer((TiledMapTileLayer)tiledMap.getLayers().get("vide")));
+        mapPuddles.putAll(getPolygonesFromLayer((TiledMapTileLayer)tiledMap.getLayers().get("puddles")));
+
+        mapObstacles.forEach((key, value)->{
+            for(float[] verticies : value){
+                new Obstacle(world, key[0], key[1], verticies);
+            }
+        });
+
+        mapPuddles.forEach((key, value)->{
+            for(float[] verticies : value){
+                //new AcidPuddle();
+                new Obstacle(world, key[0], key[1], verticies);
+            }
+        });
+    }
+
+    /**
+     * Ajoute au monde les zombies présents dans la tiledMap
+     * @param world
+     * @return le set contenant les zombies ajoutés au monde.
+     */
     public Set<Zombie> addZombies(World world){
         Set<Zombie> zombies = new HashSet<>();
         MapLayer mapLayerZombies = tiledMap.getLayers().get("Zombies");
@@ -62,11 +102,11 @@ public class LevelLoader {
         return zombies;
     }
 
-    public int  getLevelWidth() {
+    public int getLevelWidth() {
         return (int) tiledMap.getProperties().get("width");
     }
-    
-    public int  getLevelHeight() {
+
+    public int getLevelHeight() {
         return (int) tiledMap.getProperties().get("height");
     }
 }
--- the-ascend-of-pluton/core/src/java/fr/ul/theascendofpluton/model/AcidPuddle.java
+++ the-ascend-of-pluton/core/src/java/fr/ul/theascendofpluton/model/AcidPuddle.java
@@ -1,2 +1,34 @@
-package fr.ul.theascendofpluton.model;public class AcidPuddle {
+package fr.ul.theascendofpluton.model;
+
+import com.badlogic.gdx.physics.box2d.*;
+
+public class AcidPuddle {
+    public BodyDef bodyDef;
+    public Body body;
+    public static float damage;
+
+    public static float getDamage() {
+        return damage;
+    }
+
+    //x,y postion de l'acide dans le monde avec son nombre de dégâts infligé au entité
+    public AcidPuddle(World world, float x, float y,float damage){
+        bodyDef  = new BodyDef();
+        bodyDef.position.set(x, y);
+        body = world.createBody(bodyDef);
+
+        FixtureDef fixtureDef = new FixtureDef();
+        fixtureDef.isSensor = true;
+        PolygonShape shape = new PolygonShape();
+        shape.setAsBox(4f,4f);
+
+        fixtureDef.shape = shape;
+
+        body.setFixedRotation(true);
+        body.createFixture(fixtureDef).setUserData("acid");
+        body.setUserData(this);
+        shape.dispose();
+
+        this.damage=damage;
+    }
 }
